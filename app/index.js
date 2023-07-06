875e0b6a696c8676ab36d192b60fc48ddf327277
ericyilin8 <ericyilin8@gmail.com>
2023-07-06 10:52:31 -0400

    Add login fields and map

--- app/index.js
+++ app/index.js
@@ -1,14 +1,40 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Alert, Button, TextInput, View, StyleSheet, Text } from 'react-native';
 import { Link } from "expo-router";
+import { useState } from 'react';
 
 export default function App() {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  onLogin = () => {
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Login Page
-      </Text>
+    
+      <TextInput
+          value={username}
+          onChangeText={(userIn) => setUsername(userIn)}
+          placeholder={'Username'}
+          style={styles.input}
+        />
+        <TextInput
+          value={password}
+          onChangeText={(passwordIn) => setPassword(passwordIn)}
+          placeholder={'Password'}
+          secureTextEntry={true}
+          style={styles.input}
+        />
+        <Link href="/map" asChild>
+        <Button
+          title={'Login'}
+          style={styles.input}
+          onPress={onLogin}
+        />
+        </Link>
       <Link href="/register">Register</Link>
-      <Link href="/map">Login</Link>
+      
       <StatusBar style="auto" />
     </View>
   );
@@ -21,4 +47,12 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  input: {
+    width: 200,
+    height: 44,
+    padding: 10,
+    borderWidth: 1,
+    borderColor: 'black',
+    marginBottom: 10,
+  }
 });
--- app/map.js
+++ app/map.js
@@ -1,14 +1,17 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import { Link } from "expo-router";
+import MapView from 'react-native-maps';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Map Page
       </Text>
+      <MapView style={styles.map} />
       <Link href="/group">Group</Link>
       <Link href="/chat">Chat</Link>
+      <Link href="/">Logout</Link>
       <StatusBar style="auto" />
     </View>
   );
@@ -21,4 +24,8 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  map: {
+    width: '100%',
+    height: '80%',
+  }
 });
