b543c8eaa8d8ef0393a77756fd99a0af86897e44
ericyilin8 <ericyilin8@gmail.com>
2023-07-06 21:39:04 -0400

    color update

--- app/_layout.js
+++ app/_layout.js
@@ -19,6 +19,6 @@ export default function App() {
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        backgroundColor: 'turquoise'
+        backgroundColor: '#23A7E0'
     }
 })
