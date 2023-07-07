72be5216a835319b5ffac86b7def0c5a7facae21
ericyilin8 <ericyilin8@gmail.com>
2023-07-07 06:46:10 -0400

    update map ui

--- app/map.js
+++ app/map.js
@@ -2,14 +2,24 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import { Link } from "expo-router";
 import MapView from 'react-native-maps';
+import { Ionicons } from '@expo/vector-icons'; 
+import { Entypo } from '@expo/vector-icons';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <MapView style={styles.map} />
-      <Link href="/group">Group</Link>
-      <Link href="/chat">Chat</Link>
-      <Link href="/">Logout</Link>
+      <View style={styles.navigation}>
+        <Link href="/group" asChild>
+          <Ionicons name="people-circle-outline" size={36} color="white" />      
+        </Link>
+        <Link href="/chat" asChild>
+          <Entypo name="chat" size={36} color="white" />
+        </Link>
+        <Link href="/" asChild>
+          <Text style={{color: "white"}}>LOGOUT</Text>
+        </Link>
+      </View>
     </View>
   );
 }
@@ -18,10 +28,19 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     alignItems: 'center',
-    justifyContent: 'center',
+    //justifyContent: 'center',
   },
   map: {
     width: '100%',
-    height: '80%',
+    height: '90%',
+  },
+  navigation: {
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    alignItems: 'center',
+    width: '100%',
+    paddingTop: 10,
+    borderTopColor: 'white',
+    borderTopWidth: 2
   }
 });
