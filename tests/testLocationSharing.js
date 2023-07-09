2cdd190bf61bc3a4fb5ae4dde800c087164bec62
CrazyLemur <trevornlucas@gmail.com>
2023-07-08 21:28:24 -0500

    Added testscript for mock 'shareLocation' events

---
+++ tests/testLocationSharing.js
@@ -0,0 +1,29 @@
+const io = require('socket.io-client');
+
+// test constants
+
+const numUsers = 5;
+const baseLocation = { latitude: 38.8735, longitude: -99.3429 }; // base location that is tweaked to give each user different location
+
+
+for(let i=0; i<numUsers; i++) {
+  const socket = io('http://localhost:3000');
+
+  let location = {
+    coords: {
+      accuracy: 35,
+      altitude: 318.0910301208496,
+      altitudeAccuracy: 6.092679500579834,
+      heading: -1,
+      latitude: baseLocation.latitude + (Math.random() - 0.5) * 0.0145,
+      longitude: baseLocation.longitude + (Math.random() - 0.5) * 0.0145,
+      speed: -1
+    },
+    timestamp: Date.now()
+  };
+  
+  let testUsername = `testUser${i}`;
+
+  console.log(`Sending location for testUser${i}:`, location);
+  socket.emit('shareLocation', location);
+}
