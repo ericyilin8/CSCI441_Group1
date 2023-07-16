b117b4b8f3127fee2a5f5349d007217ffcd6fbf3
ericyilin8 <ericyilin8@gmail.com>
2023-07-16 18:17:15 -0400

    Set up socket global state

--- AppState.js
+++ AppState.js
@@ -3,25 +3,11 @@ import React, { useState } from 'react';
 export const AppStateContext = React.createContext();
 
 export const AppStateProvider = ({ children }) => {
-  const [globalState, setGlobalState] = useState({ count: 0 });
-
-  const incrementCount = () => {
-    setGlobalState((prevState) => ({
-      ...prevState,
-      count: prevState.count + 1,
-    }));
-  };
-
-  const decrementCount = () => {
-    setGlobalState((prevState) => ({
-      ...prevState,
-      count: prevState.count - 1,
-    }));
-  };
+  const [socket, setSocket] = useState({});
 
   return (
     <AppStateContext.Provider
-      value={{ globalState, incrementCount, decrementCount }}
+      value={{ socket, setSocket }}
     >
       {children}
     </AppStateContext.Provider>
